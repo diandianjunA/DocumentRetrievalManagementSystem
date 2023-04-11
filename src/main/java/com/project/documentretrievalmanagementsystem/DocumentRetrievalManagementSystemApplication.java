bac1bdee99fc3a78988ced1d47ade9927451febf
diandianjun <1730523754@qq.com>
2023-04-11 19:45:43 +0800

    Initial commit

---
+++ src/main/java/com/project/documentretrievalmanagementsystem/DocumentRetrievalManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.documentretrievalmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentRetrievalManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentRetrievalManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/documentretrievalmanagementsystem/DocumentRetrievalManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.documentretrievalmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentRetrievalManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
