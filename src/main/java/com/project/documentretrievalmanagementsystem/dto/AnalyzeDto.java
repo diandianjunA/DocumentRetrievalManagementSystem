0b05286fc42eb8426f61a96749e3b58dfac2f60d
diandianjun <1730523754@qq.com>
2023-05-26 17:18:00 +0800

    修改analyzeDto

--- src/main/java/com/project/documentretrievalmanagementsystem/dto/AnalyzeDto.java
+++ src/main/java/com/project/documentretrievalmanagementsystem/dto/AnalyzeDto.java
@@ -7,13 +7,13 @@ import java.util.List;
 @Data
 public class AnalyzeDto {
     private List<ProjectDto> list;
-    private int total = 0; // 总记录数
-    private int pageSize = 20; // 每页显示记录数
-    private int pages = 1; // 总页数
-    private int pageNum = 1; // 当前页
+    private int total; // 总记录数
+    private int pageSize; // 每页显示记录数
+    private int pages; // 总页数
+    private int pageNum; // 当前页
 
-    private boolean isFirstPage=false;        //是否为第一页
-    private boolean isLastPage=false;         //是否为最后一页
-    private boolean hasPreviousPage=false;   //是否有前一页
-    private boolean hasNextPage=false;       //是否有下一页
+    private boolean isFirstPage;        //是否为第一页
+    private boolean isLastPage;         //是否为最后一页
+    private boolean hasPreviousPage;   //是否有前一页
+    private boolean hasNextPage;       //是否有下一页
 }
